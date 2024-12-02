73be31396dda4755c8b8d03221dc9429b351458a
CerfSoleil <robedj12@gmail.com>
2024-12-02 17:29:09 -0600

    Add files via upload

---
+++ format.js
@@ -0,0 +1,34 @@
+import fs from 'fs';
+
+export const formatEntry = ({ name, email, gitHubName, repo, summary, documentation, installation, usage, contribution, credits, license }) => {
+    return `# ${name}\n\n
+  ## Description
+  ${summary}\n\n
+  ## Table of Contents\n
+  - [Installation](#installation)\n
+  - [Usage](#usage)\n
+  - [Documentation](#documentation)\n
+  - [Contributing](#contributing)\n
+  - [Tests](#tests)\n
+  - [License](#license)\n
+  - [Questions](#questions)\n\n
+  ## Installation\n
+  ${installation}\n\n
+  ## Usage\n
+  ${usage}\n\n
+  ## Documentation\n
+  ${documentation}\n\n
+  ## Contributing\n
+  ${contribution}\n\n
+  ## Credits\n
+  ${credits}\n
+  A link to the repositiory: ${repo}\n\n
+  ## License\n
+  This project uses the ${license} license.\n\n\n
+
+  ## Questions\n
+If you have any questions, please feel free to contact me at ${email}. You can also find my GitHub here: [${gitHubName}](https://github.com/${gitHubName}).
+`;
+}
+
+export default formatEntry;
--- index.js
+++ index.js
@@ -5,37 +5,7 @@ import inquirer from 'inquirer';
 
 // TODO: Create an array of questions for user input
 import { questions } from './utils/questions.js';
-
-const formatEntry = ({ name, email, gitHubName, repo, summary, documentation, installation, usage, contribution, credits, license }) => {
-    return `# ${name}\n\n
-  ## Description
-  ${summary}\n\n
-  ## Table of Contents\n
-  - [Installation](#installation)\n
-  - [Usage](#usage)\n
-  - [Documentation](#documentation)\n
-  - [Contributing](#contributing)\n
-  - [Tests](#tests)\n
-  - [License](#license)\n
-  - [Questions](#questions)\n\n
-  ## Installation\n
-  ${installation}\n\n
-  ## Usage\n
-  ${usage}\n\n
-  ## Documentation\n
-  ${documentation}\n\n
-  ## Contributing\n
-  ${contribution}\n\n
-  ## Credits\n
-  ${credits}\n
-  A link to the repositiory: ${repo}\n\n
-  ## License\n
-  ${license}\n\n\n
-
-  ## Questions\n
-If you have any questions, please feel free to contact me at ${email}. You can also find my GitHub here: [${gitHubName}](https://github.com/${gitHubName}).
-`;
-}
+import { formatEntry } from './utils/format.js';
 
 // TODO: Create a function to write README file
 function writeToFile(data) {
