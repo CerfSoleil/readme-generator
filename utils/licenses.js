822d08047952978cf62b74138bf7c462f3b0f69b
CerfSoleil <robedj12@gmail.com>
2024-12-02 18:20:26 -0600

    licenses.js bug fixing

--- utils/licenses.js
+++ utils/licenses.js
@@ -10,27 +10,27 @@ export const licenses = [
     },{
         name: `[Apache 2.0]`,
         value: `[Apache 2.0]`,
-        badge: `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)(https://opensource.org/licenses/Apache-2.0)`,
+        badge: `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`,
     },{
         name: `[Boost 1.0]`,
         value: `[Boost 1.0]`,
-        badge: `![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)(https://www.boost.org/LICENSE_1_0.txt)`,
+        badge: `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`,
     },{
         name: `[BSD 2-Clause]`,
         value: `[BSD 2-Clause]`,
-        badge: `![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`,
+        badge: `[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`,
     },{
         name: `[BSD 3-Clause]`,
         value: `[BSD 3-Clause]`,
-        badge: `![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)(https://opensource.org/licenses/BSD-3-Clause)`,
+        badge: `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`,
     },{
         name: `[Creative Commons Zero 1.0]`,
         value: `[Creative Commons Zero 1.0]`,
-        badge: `![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)(http://creativecommons.org/publicdomain/zero/1.0/)`,
+        badge: `[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)`,
     },{
         name: `[Creative Commons Attribution 4.0]`,
         value: `[Creative Commons Attribution 4.0]`,
-        badge: `![License: CC BY 4.0](https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg)(https://creativecommons.org/licenses/by/4.0/)`,
+        badge: `[![License: CC BY 4.0](https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)`,
     },{
         name: `[Creative Commons Attribution Share-Alike 4.0]`,
         value: `[Creative Commons Attribution Share-Alike 4.0]`,
@@ -38,95 +38,95 @@ export const licenses = [
     },{
         name: `[Creative Commons Attribution Non-Commercial 4.0]`,
         value: `[Creative Commons Attribution Non-Commercial 4.0]`,
-        badge: `![License: CC BY-NC 4.0](https://img.shields.io/badge/License-CC_BY--NC_4.0-lightgrey.svg)(https://creativecommons.org/licenses/by-nc/4.0/)`,
+        badge: `[![License: CC BY-NC 4.0](https://img.shields.io/badge/License-CC_BY--NC_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc/4.0/)`,
     },{
         name: `[Creative Commons Attribution No-Derivatives 4.0]`,
         value: `[Creative Commons Attribution No-Derivatives 4.0]`,
-        badge: `![License: CC BY-ND 4.0](https://img.shields.io/badge/License-CC_BY--ND_4.0-lightgrey.svg)(https://creativecommons.org/licenses/by-nd/4.0/)`,
+        badge: `[![License: CC BY-ND 4.0](https://img.shields.io/badge/License-CC_BY--ND_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nd/4.0/)`,
     },{
         name: `[Creative Commons Attribution Non-Commercial Share-Alike 4.0]`,
         value: `[Creative Commons Attribution Non-Commercial Share-Alike 4.0]`,
-        badge: `![License: CC BY-NC-SA 4.0](https://img.shields.io/badge/License-CC_BY--NC--SA_4.0-lightgrey.svg)(https://creativecommons.org/licenses/by-nc-sa/4.0/)`,
+        badge: `[![License: CC BY-NC-SA 4.0](https://img.shields.io/badge/License-CC_BY--NC--SA_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0/)`,
     },{
         name: `[Creative Commons Attribution Non-Commercial No-Derivatives 4.0]`,
         value: `[Creative Commons Attribution Non-Commercial No-Derivatives 4.0]`,
-        badge: `![License: CC BY-NC-ND 4.0](https://img.shields.io/badge/License-CC_BY--NC--ND_4.0-lightgrey.svg)(https://creativecommons.org/licenses/by-nc-nd/4.0/)`,
+        badge: `[![License: CC BY-NC-ND 4.0](https://img.shields.io/badge/License-CC_BY--NC--ND_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-nd/4.0/)`,
     },{
         name: `[Eclipse Public 1.0]`,
         value: `[Eclipse Public 1.0]`,
-        badge: `![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)(https://opensource.org/licenses/EPL-1.0)`,
+        badge: `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`,
     },{
         name: `[GNU General Public 2.0]`,
         value: `[GNU General Public 2.0]`,
-        badge: `![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)(https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`,
+        badge: `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`,
     },{
         name: `[GNU General Public 3.0]`,
         value: `[GNU General Public 3.0]`,
-        badge: `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)(https://www.gnu.org/licenses/gpl-3.0)`,
+        badge: `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`,
     },{
         name: `[GNU Affero Public 3.0]`,
         value: `[GNU Affero Public 3.0]`,
-        badge: `![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)(https://www.gnu.org/licenses/agpl-3.0)`,
+        badge: `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`,
     },{
         name: `[GNU Lesser General Public 3.0]`,
         value: `[GNU Lesser General Public 3.0]`,
-        badge: `![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)(https://www.gnu.org/licenses/lgpl-3.0)`,
+        badge: `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)`,
     },{
         name: `[GNU Free Documentation 1.3]`,
         value: `[GNU Free Documentation 1.3]`,
-        badge: `![License: FDL 1.3](https://img.shields.io/badge/License-FDL_v1.3-blue.svg)(https://www.gnu.org/licenses/fdl-1.3)`,
+        badge: `[![License: FDL 1.3](https://img.shields.io/badge/License-FDL_v1.3-blue.svg)](https://www.gnu.org/licenses/fdl-1.3)`,
     },{
         name: `[OES Hippocratic 2.1]`,
         value: `[OES Hippocratic 2.1]`,
-        badge: `![License: Hippocratic 2.1](https://img.shields.io/badge/License-Hippocratic_2.1-lightgrey.svg)(https://firstdonoharm.dev)`,
+        badge: `[![License: Hippocratic 2.1](https://img.shields.io/badge/License-Hippocratic_2.1-lightgrey.svg)](https://firstdonoharm.dev)`,
     },{
         name: `[OES Hippocratic 3.0]`,
         value: `[OES Hippocratic 3.0]`,
-        badge: `![License: Hippocratic 3.0](https://img.shields.io/badge/License-Hippocratic_3.0-lightgrey.svg)(https://firstdonoharm.dev)`,
+        badge: `[![License: Hippocratic 3.0](https://img.shields.io/badge/License-Hippocratic_3.0-lightgrey.svg)](https://firstdonoharm.dev)`,
     },{
         name: `[IBM Public 1.0]`,
         value: `[IBM Public 1.0]`,
-        badge: `![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)(https://opensource.org/licenses/IPL-1.0)`,
+        badge: `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)`,
     },{
         name: `[ISC]`,
         value: `[ISC]`,
-        badge: `![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)(https://opensource.org/licenses/ISC)`,
+        badge: `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`,
     },{
         name: `[MIT]`,
         value: `[MIT]`,
-        badge: `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)(https://opensource.org/licenses/MIT)`,
+        badge: `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
     },{
         name: `[Mozilla Public 2.0]`,
         value: `[Mozilla Public 2.0]`,
-        badge: `![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)(https://opensource.org/licenses/MPL-2.0)`,
+        badge: `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`,
     },{
         name: `[Open Data Commons: Attribution]`,
         value: `[Open Data Commons: Attribution]`,
-        badge: `![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)(https://opendatacommons.org/licenses/by/)`,
+        badge: `[![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)](https://opendatacommons.org/licenses/by/)`,
     },{
         name: `[Open Data Commons: Open Database]`,
         value: `[Open Data Commons: Open Database]`,
-        badge: `![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)(https://opendatacommons.org/licenses/odbl/)`,
+        badge: `[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)`,
     },{
         name: `[Open Data Commons: Public Domain Dedication]`,
         value: `[Open Data Commons: Public Domain Dedication]`,
-        badge: `![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)(https://opendatacommons.org/licenses/pddl/)`,
+        badge: `[![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)](https://opendatacommons.org/licenses/pddl/)`,
     },{
         name: `[Artistic (Perl) 1.0]`,
         value: `[Artistic (Perl) 1.0]`,
-        badge: `![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)(https://opensource.org/licenses/artistic-perl-1-0-2)`,
+        badge: `[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/artistic-perl-1-0-2)`,
     },{
         name: `[Artistic 2.0]`,
         value: `[Artistic 2.0]`,
-        badge: `![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg)(https://opensource.org/licenses/Artistic-2.0)`,
+        badge: `[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)`,
     },{
         name: `[Open Font 1.1]`,
         value: `[Open Font 1.1]`,
-        badge: `![License: Open Font-1.1](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)(https://opensource.org/licenses/OFL-1.1)`,
+        badge: `[![License: Open Font-1.1](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)](https://opensource.org/licenses/OFL-1.1)`,
     },{
         name: `[Unlicense]`,
         value: `[Unlicense]`,
-        badge: `![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)(http://unlicense.org/)`,
+        badge: `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`,
     },{
         name: `[Do What the Fuck You Want Public Liscence]`,
         value: `[Do What the Fuck You Want Public Liscence]`,
